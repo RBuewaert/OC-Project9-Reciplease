393a74254e39e7c7f1c660ce2df63d8b417553a7
Romain Buewaert <romain.buewaert@gmail.com>
2021-10-08 12:29:37 +0200

    update gitignore

---
+++ Reciplease/Supporting Files/KeyTest.swift
@@ -0,0 +1,13 @@
+//
+//  KeyTest.swift
+//  Reciplease
+//
+//  Created by Romain Buewaert on 08/10/2021.
+//
+
+import Foundation
+
+final class KeyTest {
+    static let key = "test"
+    static let id = "test"
+}
